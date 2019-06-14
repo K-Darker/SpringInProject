95eac65d1cb7e831a76fff620d13aa9628a60631
周浩 <HaoZhou2@sf-express.com>
2019-06-14 17:12:03 +0800

    Spring集成zk案例

---
+++ springcloud-zk/src/test/java/springcloudzk/springcloudzk/SpringcloudZkApplicationTests.java
@@ -0,0 +1,16 @@
+package springcloudzk.springcloudzk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcloudZkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
